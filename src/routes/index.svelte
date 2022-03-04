ba7840d8d354e97a844f435fee1e4ef74d44993c
Ryan Gossiaux <ryan.gossiaux@gmail.com>
2022-03-03 22:02:12 -0800

    Update example

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,2 +1,11 @@
+<script>
+	import { onMount } from 'svelte';
+	onMount(async () => {
+		await fetch('http://localhost:5050', {
+			credentials: 'include'
+		});
+	});
+</script>
+
 <h1>Welcome to SvelteKit</h1>
 <p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,6 +1,8 @@
 /** @type {import('./[id]').RequestHandler} */
 export async function get({ params }) {
-    await fetch("http://localhost:5050");
+    await fetch("http://localhost:5050", {
+        credentials: "include",
+    });
     return {
         status: 200,
         headers: {
